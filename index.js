8884865f9ed0f3d25294e779ea1fa2dcdfb41ef1
Leon K <leonknight@outlook.com>
2018-02-23 06:04:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name)
+{
+  // return position in line
+}
+
+function nowServing
+{
+  // return the first person in line and remove them
+  else 
+  {
+    console.log("There is nobody waiting to be served")
+  }  
+}
+
+function current
