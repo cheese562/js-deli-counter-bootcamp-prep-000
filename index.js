3a7a403c5c59277e283c8bcf2075b6e0b356f782
Leon K <leonknight@outlook.com>
2018-02-23 06:20:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,19 @@ function nowServing(katzDeliLine)
   }  
 }
 
-function currentLine(line)
+function currentLine(katzDeliLine)
 {
-  if (line.length === 0)
+  if (katzDeliLine.length === 0)
   {
     return "The line is currently empty."
   }
   else 
   {
+    
+    
+    
+    
+    
    return `The line is currently:
   }
 }
