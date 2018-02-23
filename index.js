2435ec6fc86a578dc10b9600bc56cf267deca9f0
Leon K <leonknight@outlook.com>
2018-02-23 06:25:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function currentLine(katzDeliLine)
     let i = 0
     while (i < katzDeliLine.length)
     {
-      line.push(` ` +katzDeliLine[i])
+      line.push(` ` + [i+1] + `. ` + katzDeliLine[i])
       i++
     }
     
